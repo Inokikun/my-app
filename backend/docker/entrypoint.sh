6ae440dc1084e77c4283e78d88f09041cd71fea4
Inokikun <spck85n9@tuba.ocn.ne.jp>
2026-08-14 14:24:26 +0900

    検証用の変更

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
+set -e
 
 echo "🚀 Starting Laravel..."
 
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-# キャッシュクリア（安全）
 php artisan config:clear
 php artisan cache:clear
 
-# マイグレーション
 php artisan migrate --force
 
-# キャッシュ生成（ここが重要）
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Nginxポート変更
 sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/sites-available/default
 
-# 起動
 php-fpm -D
 nginx -g "daemon off;"
