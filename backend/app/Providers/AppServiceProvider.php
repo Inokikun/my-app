fb5a89e8c2de8ac438804f54d8cd6e63bdced62b
Inokikun <spck85n9@tuba.ocn.ne.jp>
2026-08-14 13:59:45 +0900

    **

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (config('app.env') === 'staging'||
+        if (config('app.env') === 'production'||
         config('app.env') === 'staging') {
         URL::forceScheme('https');
     }
